1de681f6d344c8345dd62385099c4f75f3bf8236
Jakub Rudnicki <hyperek@vp.pl>
2023-10-31 17:24:43 +0100

    Init App and Doc

---
+++ App/App/App.h
@@ -0,0 +1,8 @@
+﻿// App.h: Header file included for standard system header files
+// or project-specific header files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Include additional headers required by the program at this point.
