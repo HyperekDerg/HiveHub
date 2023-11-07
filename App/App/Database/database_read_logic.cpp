d7771d108e6209c471f9fa5c35ffc98879a81cf1
Jakub Rudnicki <hyperek@vp.pl>
2023-11-07 14:51:38 +0100

    Read-Database-Logic-Structure

---
+++ App/App/Database/database_read_logic.cpp
@@ -0,0 +1,65 @@
+#include "database_controller.h"
+#include "line_parser.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+ReadDatabase::ReadDatabase(const WriteDatabase& writeDatabase) : writeDatabase_(writeDatabase) {}
+
+void ReadDatabase::displayUser(const UserData& userData) {
+	cout << "Email: " << userData.email << endl;
+	cout << "First Name: " << userData.firstName << endl;
+	cout << "Last Name: " << userData.lastName << endl;
+	cout << "Address: " << userData.address << endl;
+	cout << "Interests: ";
+	for (const string& interest : userData.interests) {
+		cout << interest << ", ";
+	}
+	cout << endl;
+}
+
+bool ReadDatabase::findUserByEmail(const string& email) {
+	const map<string, UserData>& userMap = writeDatabase_.getUserMap();
+	auto it = userMap.find(email);
+
+	if (it != userMap.end()) {
+		displayUser(it->second);
+		return true;
+	}
+	else {
+		cout << "User with email " << email << " not found in the database." << endl;
+		return false;
+	}
+}
+
+bool ReadDatabase::findUsersByInterests(const vector<string>& interests) {
+	const map<string, UserData>& userMap = writeDatabase_.getUserMap();
+	bool found = false;
+	LineParser lineParser;
+
+	for (const auto& pair : userMap) {
+		const UserData& userData = pair.second;
+
+		bool hasInterests = true;
+
+		// Check if the interests provided by the user are a subset of the user's interests
+		for (const string& interest : interests) {
+			if (find(userData.interests.begin(), userData.interests.end(), interest) == userData.interests.end()) {
+				hasInterests = false;
+				break;
+			}
+		}
+
+		if (hasInterests) {
+			displayUser(userData);
+			found = true;
+		}
+	}
+
+	if (!found) {
+		cout << "No users found with the specified interests." << endl;
+	}
+
+	return found;
+}
